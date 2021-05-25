79219ac1a6685b402235db0a2aadd087e221fab3
anggitajati <anggitajati4@gmail.com>
2021-05-25 03:07:33 -0700

    menyelesaikan ui autentikasi resolve #1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:uas_final/pages/home_page.dart';
+import 'package:uas_final/pages/signup_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: SignUpPage(),
     );
   }
 }
